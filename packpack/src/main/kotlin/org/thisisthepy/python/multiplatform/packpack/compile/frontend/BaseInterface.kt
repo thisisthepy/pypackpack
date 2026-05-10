f5faeb72701544e695b1510a8c21422c193350fa
rnoro <rnoro5122@gmail.com>
2026-05-10 19:32:02 +0900

    Feat: Add compile frontend

--- packpack/src/main/kotlin/org/thisisthepy/python/multiplatform/packpack/compile/frontend/BaseInterface.kt
+++ packpack/src/main/kotlin/org/thisisthepy/python/multiplatform/packpack/compile/frontend/BaseInterface.kt
@@ -1,5 +1,33 @@
 package org.thisisthepy.python.multiplatform.packpack.compile.frontend
 
+import org.thisisthepy.python.multiplatform.packpack.compile.middleware.BaseInterface as MiddlewareInterface
+
+/**
+ * Frontend type enum
+ */
+enum class FrontendType {
+    CLI,
+    GRADLE,
+}
+
 /**
  * Factory pattern base interface for compilation frontend
  */
+interface BaseInterface {
+    fun initialize()
+
+    fun getMiddleware(): MiddlewareInterface
+
+    companion object {
+        /**
+         * Create frontend instance
+         * @param type Frontend type
+         * @return Frontend instance
+         */
+        fun create(type: FrontendType): BaseInterface =
+            when (type) {
+                FrontendType.CLI -> Cli()
+                FrontendType.GRADLE -> Gradle()
+            }
+    }
+}
--- packpack/src/main/kotlin/org/thisisthepy/python/multiplatform/packpack/compile/frontend/Cli.kt
+++ packpack/src/main/kotlin/org/thisisthepy/python/multiplatform/packpack/compile/frontend/Cli.kt
@@ -1,5 +1,29 @@
 package org.thisisthepy.python.multiplatform.packpack.compile.frontend
 
+import org.thisisthepy.python.multiplatform.packpack.compile.middleware.DefaultInterface
+import org.thisisthepy.python.multiplatform.packpack.compile.middleware.BaseInterface as MiddlewareInterface
+
 /**
  * CLI interface for compilation process
  */
+class Cli : BaseInterface {
+    private lateinit var middleware: MiddlewareInterface
+
+    /**
+     * Initialize CLI interface
+     */
+    override fun initialize() {
+        middleware = DefaultInterface()
+        middleware.initialize()
+    }
+
+    /**
+     * Get middleware interface
+     */
+    override fun getMiddleware(): MiddlewareInterface {
+        if (!::middleware.isInitialized) {
+            initialize()
+        }
+        return middleware
+    }
+}
